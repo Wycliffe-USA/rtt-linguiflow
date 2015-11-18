1de565f0a8d8eaf882d2077c516520f984ed09d2
Bren Briggs <briggs.brenton@gmail.com>
2015-11-17 21:38:44 -0500

    Dumping stuff because reasons

--- Vagrantfile
+++ Vagrantfile
@@ -18,7 +18,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-Vagrant.configure(2) do |config|
+Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
+  config.vm.provision "file", source: "getcorpora.sh", destination: "/tools/getcorpora.sh"
   config.vm.provision "shell", path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,14 @@ mkdir /mgiza && cd /mgiza
 git clone https://github.com/moses-smt/mgiza.git
 cd ./mgiza/mgizapp
 cmake . && make && make install
+mkdir /tools
+echo `whoami`
+mkdir -p /tools/bin/training-tools
+export BINDIR=/tools/bin/training-tools
+sudo rsync -arO ./bin/* $BINDIR/mgizapp
+sudo cp scripts/merge_alignment.py $BINDIR
+cd /tools
+wget http://www.statmt.org/europarl/v5/tools.tgz && tar -xf ./tools.tgz
+rm -f ./tools.tgz
+cd /tools
+chmod +x ./getcorpora.sh
