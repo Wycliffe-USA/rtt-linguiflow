161b983562e4e5c5efaebc538efe43f882ec1445
Bren Briggs <briggs.brenton@gmail.com>
2015-12-01 13:22:44 -0500

    Commenting mgiza bootstrap for clarity

--- mgiza/bootstrap.sh
+++ mgiza/bootstrap.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 apt-get update
-apt-get install -y git make build-essential clang libboost-all-dev cmake python-pip python-scipy python-numpy
+
+#Install mgiza prereqs
+apt-get install -y git make build-essential clang libboost-all-dev cmake 
+
+#Install gensim/NLTK prereqs
+apt-get install -y python-pip python-scipy
 pip install -U nltk
 pip install -U gensim
+
+#Compile mgiza
 mkdir /mgiza && cd /mgiza
 git clone https://github.com/moses-smt/mgiza.git
 cd ./mgiza/mgizapp
 cmake . && make && make install
+
+#post-compile setup
 mkdir /home/vagrant/tools && chown vagrant:vagrant /home/vagrant/tools
 mkdir -p /home/vagrant/tools/bin/training-tools
 export BINDIR=/home/vagrant/tools/bin/training-tools
 sudo rsync -arO ./bin/* $BINDIR/mgizapp
 sudo cp scripts/merge_alignment.py $BINDIR
+
+#Grab Europarl tools for aligning and cleaning data
 cd /home/vagrant/tools
 wget http://www.statmt.org/europarl/v5/tools.tgz && tar -xf ./tools.tgz
 rm -f ./tools.tgz
 cd /home/vagrant/tools
+
+#Make our own scripts executable
 chmod +x ./*.sh
