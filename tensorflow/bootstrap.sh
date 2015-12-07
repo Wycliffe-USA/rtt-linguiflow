036eb0702f077b999cc7a504270599e6fef7193d
Bren Briggs <briggs.brenton@gmail.com>
2015-12-07 11:47:12 -0500

    Downloading only selected corpus data sets for NLTK.

--- tensorflow/bootstrap.sh
+++ tensorflow/bootstrap.sh
@@ -4,6 +4,6 @@ apt-get install -y python-dev python-pip python-scipy
 pip install -U nltk
 pip install -U gensim
 pip install -U https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
-mkdir /home/vagrant/tools && chown vagrant:vagrant /home/vagrant/tools
+chown vagrant:vagrant /home/vagrant/tools
 cd /home/vagrant/tools
-chmod +x ./*.sh
+chmod +x home/vagrant/tools/*.sh
