98fe3adea3a4d141e62a54be93c50ed3be7dbc3f
Bren Briggs <briggs.brenton@gmail.com>
2015-12-07 16:47:42 -0500

    Skipping test.sh, doing it in travis.yml now

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-cd ./mgiza
-sudo bash ./bootstrap.sh
-sudo bash ./getcorpora.sh
-sudo bash ./tokenize.sh
+# #!/bin/bash
+# cd ./mgiza
+# sudo bash ./getcorpora.sh
+# sudo bash ./tokenize.sh
