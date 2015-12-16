cd7a312222cc2e26c39e1b06612d0ac8b7b3c397
Bren Briggs <briggs.brenton@gmail.com>
2015-12-16 14:47:43 -0500

    Add INSTALL_HOME param to to getcorpora to direct location of corpus files

--- mgiza/getcorpora.sh
+++ mgiza/getcorpora.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #getcorpora.sh
-cd
+INSTALL_HOME=$1
+if [ -z "${INSTALL_HOME}" ]; then
+    echo "USAGE: ./tokenize.sh app_root"
+    echo "Example: ./tokenize.sh ${HOME}"
+    exit 1
+fi
+cd ${INSTALL_HOME}
 mkdir ./corpora
 cd ./corpora
 wget https://s3.amazonaws.com/briggsb/de-en.tgz
