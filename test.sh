40f405e67e6b7bf6ce0f5063d3d03766fbe7eee0
Bren Briggs <briggs.brenton@gmail.com>
2015-12-22 08:39:31 -0500

    Change approot for TravisCI testing

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 pip install -U nltk
 pip install -U gensim
 python -m nltk.downloader punkt
-sudo bash ./mgiza/tools/getcorpora.sh ${HOME}
+sudo bash ./mgiza/tools/getcorpora.sh ${HOME}/mgiza
 sudo bash ./mgiza/tools/en-sp_tokenize.sh ${HOME}/mgiza
