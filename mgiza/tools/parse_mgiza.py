2501898be9e91ccc3dfe15b70a0c2988d06ab4c4
iblaauw <naibla8358@gmail.com>
2015-12-30 16:13:13 -0500

    Finished the parse script for the mgiza output

--- mgiza/tools/parse_mgiza.py
+++ mgiza/tools/parse_mgiza.py
@@ -1,36 +1,81 @@
-fname = 'example_output.txt'
+import re
+
+fname = 'sample_out'
 
 def parse_alignment(alignment):
-    alignment = alignment.split('\n')
-    score = float(alignment[0].split()[-1])
-    result = alignment[2].split()
+    raw_lines = alignment.split('\n')
+    lines = remake_lines(raw_lines)
 
-    trg_phrase = alignment[1].split()
+    print lines[0]
+    print lines[1]
+    print lines[2]
 
-    for i, src_word in enumerate(result[::2]):
-        try:  # Handle bad inputs
-            int(src_word)
-            return
-        except ValueError:
-            pass
+    score = get_score(lines[0])
 
-        trg_words = []
-        trg_word_positions = result[i + 1].split()  # e.g., ['({', '11', '16', '17', '})']
+    i_raw = get_indexes_raw(lines[2])
+    indexes = get_indexes(i_raw)
 
-        # Parse out the target words for each source word
-        for pos in trg_word_positions:
-            try:
-                pos = int(pos)
-            except ValueError:
-                pos = None
+    w_raw = get_words_raw(lines[2])
+    words_src = get_words(w_raw)
 
-            if pos:
-                trg_words.append(trg_phrase[pos - 1])
+    words_targ = get_words(lines[1].split())
 
-        # Add entries to the DB
-        if trg_words:
-            print 'Source Word:', src_word
-            print 'Target Words:', trg_words
+    print "\tScore:", score
+    print "\tSource Words:", words_src
+    print "\tTarget Words:", words_targ
+    print "\tIndexes:", indexes
+
+def get_score(line):
+    regstr = r'.*: ([\d.e-]+)\s*'
+    match = re.match(regstr, line)
+    assert(match != None)
+    score = match.group(1)
+    return float(score)
+
+def get_indexes_raw(line):
+    regstr = r'\({([\d ]*)}\)'
+    matches = re.findall(regstr, line)
+    assert(matches != None)
+    return matches
+
+def get_indexes(indexes_raw):
+    indexes = []
+    for ind_str in indexes_raw:
+        ind_str = ind_str.strip()
+        vals = map(int, ind_str.split())
+        indexes.append(vals)
+
+    return indexes
+
+def get_words_raw(line):
+    regstr = r'(\S+) \(.*?\)'
+    matches = re.findall(regstr, line)
+    assert(matches != None)
+    return matches
+
+def get_words(words_raw):
+    words = [ w.strip() for w in words_raw ]
+    return words
+
+
+# Make it so that all of the target string is on one line
+#   and all of the target data is also on one line
+# Returns a list of lines
+def remake_lines(lines):
+    new_lines = []
+    new_lines.append(lines[0])
+
+    for index, line in enumerate(lines):
+        if line.startswith("NULL"):
+            break
+
+    full_str = ' '.join(lines[1:index])
+    alignment_str = ' '.join(lines[index:])
+
+    new_lines.append(full_str)
+    new_lines.append(alignment_str)
+
+    return new_lines
 
 def parse_output(mgiza_file):
     """Given an a file of raw mgiza output, return a list of results:
