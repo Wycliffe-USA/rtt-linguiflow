2e379500e79f95c53e98cfb6a0546df09740e3c4
leigh schrandt <leigh@programmer.net>
2015-12-31 04:56:41 -0600

    added example model for translation in the frontend dependencies for direct manipulation

---
+++ webserver/data/hackstack/public/javascript/translate-model.js
@@ -0,0 +1,48 @@
+var example_model = {
+  "English": {
+    "Spanish": {
+      "all": [
+        {alignment:['todas'], confidence: 73},
+        {alignment:['todos'], confidence: 33},
+      ],
+      "the": [
+        {alignment:['las'], confidence: 82},
+        {alignment:['los'], confidence: 68},
+      ],
+      "nations": [
+        {alignment:['naciones'], confidence: 90},
+        {alignment:['gentes'], confidence: 13},
+      ]
+    }
+    "French": {
+      "all": [
+        {alignment:['toutes'], confidence: 76},
+        {alignment:['tous'], confidence: 71},
+        {alignment:['nous, sommes'], confidence: 11},
+      ],
+      "the": [
+        {alignment:['les'], confidence: 93},
+        {alignment:['le'], confidence: 51},
+      ],
+      "nations": [
+        {alignment:['nations'], confidence: 49},
+        {alignment:['peuples'], confidence: 40},
+      ]
+    }
+  }
+  "French": {
+    "German": {
+      "toutes": [
+        {alignment:['sie alle'], confidence: 46},
+        {alignment:['alle'], confidence: 21}
+      ],
+      "les": [
+        {alignment:[], confidence: 82},
+      ],
+      "nations": [
+        {alignment:['nationen'], confidence: 45},
+        {alignment:['volker'], confidence: 44},
+      ]
+    }
+  }
+};
