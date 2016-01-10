ff68ef7baff5a624b122b1e594039dc534cfe537
Bren Briggs <briggs.brenton@gmail.com>
2016-01-09 23:15:48 -0500

    Point test to new configfile

--- mgiza/tests/train.sh
+++ mgiza/tests/train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 WORKINGDIR=$(pwd)
 sudo bash ./mgiza/tools/getcorpora.sh ${WORKINGDIR}/mgiza
-sudo bash ./mgiza/tools/en-sp-align_words.sh ${WORKINGDIR}/mgiza ${WORKINGDIR}/mgiza/tools/travis_mgiza_configfile
+sudo bash ./mgiza/tools/generate_mgiza_configfile.sh ${WORKINGDIR}/mgiza
+sudo bash ./mgiza/tools/en-sp-align_words.sh ${WORKINGDIR}/mgiza ${WORKINGDIR}/mgiza/tools/mgiza_configfile
