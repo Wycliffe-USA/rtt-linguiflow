25f88d85077c7ff7232d5b0b933be8ad021f5bb9
Bren Briggs <briggs.brenton@gmail.com>
2016-01-11 16:20:25 -0500

    Refactor webserver vagrant env to build and start webapp unattended

---
+++ webserver/puppet/manifests/default.pp
@@ -0,0 +1,54 @@
+class { '::nodejs':
+  manage_package_repo       => false,
+  nodejs_dev_package_ensure => 'present',
+  npm_package_ensure        => 'present',
+}
+
+package{'bower':
+  ensure      => 'installed',
+  provider    => 'npm',
+  require     => Class['::nodejs'],
+}
+
+file {'/usr/bin/node':
+  ensure      => 'link',
+  target      => '/usr/bin/nodejs',
+  require     => Class['::nodejs'],
+}
+
+
+package { 'git':
+  ensure  => installed,
+  require => File['/usr/bin/node'],
+}
+
+exec { 'npm i':
+  cwd         => '/var/www/project/hackstack',
+  creates     => '/var/www/project/hackstack/node_modules/express/History.md',
+  path        => ['/usr/bin'],
+  require     => Class['::nodejs'],
+}
+
+exec {'bower install':
+  command     => '/usr/local/bin/bower install --config.interactive=false --allow-root',
+  cwd         => '/var/www/project/hackstack',
+  creates     => '/var/www/project/hackstack/bower_components/bootstrap/README.md',
+  path        => ['/usr/bin'],
+  require     => Package['git'],
+}
+
+service { 'linguiflow':
+  ensure => running,
+  provider => 'upstart',
+  require => Exec['bower install'],
+}
+
+class {'nginx': }
+
+class {'postgresql::server': }
+
+postgresql::server::db { 'linguiflow_db':
+  user          => 'linguiflow',
+  password      => postgresql_password('linguiflow', 'linguiflow'),
+  require       => Class['postgresql::server']
+}
