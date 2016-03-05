79c9b5481cf8a3dc323e719d3faeccbc77945761
leigh schrandt <leigh@programmer.net>
2016-03-05 14:56:41 -0700

    added CouchDB credentials for unauthed, local instance

--- webserver/data/hackstack/config.js
+++ webserver/data/hackstack/config.js
@@ -7,6 +7,9 @@ config.client = {
   // front-end config values
 }
 
+// CouchDB Authentication
+config.couchdbadmin = process.env.COUCHDB_ADMIN || ''; //'admin:secret' + '@';
+config.couchdb      = process.env.COUCHDB || 'http://' + config.couchdbadmin + 'localhost:5984/';
 
 
 module.exports = config;
